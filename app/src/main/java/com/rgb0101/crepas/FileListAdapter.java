46ff9e9eb02c566d0f9441b4935a872c8159f120
0101rgb <0101rgb@gmail.com>
2015-11-23 10:08:43 +0900

    add button img

--- app/src/main/java/com/rgb0101/crepas/FileListAdapter.java
+++ app/src/main/java/com/rgb0101/crepas/FileListAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,11 +30,14 @@ public class FileListAdapter extends ArrayAdapter {
         if( v == null ) v= LayoutInflater.from(mContext).inflate(R.layout.item_filelist, null);
 
         if(mList != null && mList.get(pos) != null ) {
-//            ((TextView)v.findViewById(R.id.textFileListItem)).setText(mList.get(pos).getFilename());
-            if (mList.get(pos).isDir())
+            if (mList.get(pos).isDir()){
+                ((ImageView)v.findViewById(R.id.imageFileListItemFolder)).setVisibility(View.VISIBLE);
                 ((TextView) v.findViewById(R.id.textFileListItem)).setText(Html.fromHtml("<b>" + mList.get(pos).getFilename() + "</b>"));
-            else
+            }
+            else{
+                ((ImageView)v.findViewById(R.id.imageFileListItemFolder)).setVisibility(View.INVISIBLE);
                 ((TextView) v.findViewById(R.id.textFileListItem)).setText(mList.get(pos).getFilename());
+            }
 //                ((TextView)v.findViewById(R.id.textFileListItem)).setTypeface(null, Typeface.BOLD);
         }
 
